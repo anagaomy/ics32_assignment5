6e0822b3ee1655edc94e37bc5c0c6a7253713bc8
Ana Gao <gaomy@uci.edu>
2024-03-03 01:13:52 -0800

    modification on OpenWeather class and error handle

--- OpenWeather.py
+++ OpenWeather.py
@@ -10,79 +10,79 @@ import json
 import urllib.request
 import urllib.error
 import requests.exceptions
-
-
-def _download_url(url_to_download: str) -> dict:
-    response = None
-    r_obj = None
-
-    try:
-        response = urllib.request.urlopen(url_to_download)
-        json_results = response.read()
-        r_obj = json.loads(json_results)
-
-    except urllib.error.HTTPError as e:
-        print("Failed to download contents of URL")
-        print("Status code: {}".format(e.code))
-
-    finally:
-        if response != None:
-            response.close()
-    
-    return r_obj
+import datetime
 
 
 class OpenWeather:
 
-    def set_apikey(self, apikey:str) -> None:
-        '''
-        Sets the apikey required to make requests to a web API.
-        :param apikey: The apikey supplied by the API service
-            
-        '''
-        #TODO: assign apikey value to a class data attribute that can be accessed by class members
-        pass
+    def __init__(self, zipcode, ccode):
+        self.zipcode = zipcode
+        self.ccode = ccode
 
+    def set_apikey(self, apikey:str) -> None:
+        self.apikey = apikey
 
     def load_data(self) -> None:
-        '''
-        Calls the web api using the required values and stores the response in class data attributes.
-            
-        '''
-        #TODO: use the apikey data attribute and the urllib module to request data from the web api. See sample code at the begining of Part 1 for a hint.
-        #TODO: assign the necessary response data to the required class data attributes
+        response = None
+        r_obj = None
+        request_url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{self.ccode}&appid={self.apikey}"
 
-        try: 
-            pass
+        try:
+            response = urllib.request.urlopen(request_url)
+            json_results = response.read()
+            r_obj = json.loads(json_results)
+
+            self.temperature = r_obj["main"]["temp"]
+            self.high_temperature = r_obj["main"]["temp_max"]
+            self.low_temperature = r_obj["main"]["temp_min"]
+            self.longitude = r_obj["coord"]["lon"]
+            self.latitude = r_obj["coord"]["lat"]
+            self.description = r_obj["weather"]["description"]
+            self.humidity = r_obj["main"]["humidity"]
+            self.city = r_obj["name"]
+            self.sunset = datetime.datetime.fromtimestamp(r_obj["sys"]["sunset"]).strftime('%H:%M')   
 
         except requests.exceptions.ConnectionError:
             raise Exception("Please check your internet connection")
 
-        except urllib.error.HTTPError:
-            raise Exception("City not found, please try again.")
+        except urllib.error.HTTPError as e:
+            status_code = e.code
+
+            if status_code == 404:
+                raise Exception("The specified URL is not valid. Please check your zipcode and country code.")
+            elif status_code == 503:
+                raise Exception("The remote API is unavailable. Please try again later.")
+            else:
+                raise Exception("An unknown error has occurred. Please try again later.")
 
         except urllib.error.URLError:
             raise Exception("Cannot connect to the internet. Please check your connection and try again.")
 
         except json.decoder.JSONDecodeError:
             raise Exception("Invalid data from remote API.")
+        
+        finally:
+            if response != None:
+                response.close()
+
+        return r_obj
 
 
-def main() -> None:
-    zip = "92697"
-    ccode = "US"
-    apikey = "071481e600ad1194c116a0b9e95d56a8"
-    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
+# def main() -> None:
+    # zip = "92697"
+    # ccode = "US"
+    # apikey = "071481e600ad1194c116a0b9e95d56a8"
+    # url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
 
-    try:
-        weather_obj = _download_url(url)
-        if weather_obj is not None:
-            print(weather_obj['weather'][0]['description'])
+    # try:
+        # weather_obj = OpenWeather(zip, ccode)
+        # if weather_obj is not None:
+            # print(weather_obj['weather'][0]['description'])
             # print(weather_obj)
 
-    except Exception as e:
-        print("Error:", e)
+    # except Exception as e:
+        # print("Error:", e)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+    # main()
